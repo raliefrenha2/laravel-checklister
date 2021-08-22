c583afadf0fe61c78672e83ecdf4ead3a8cef295
Romi Alief Rahman <raliefrenha2@gmail.com>
2021-08-22 09:17:56 +0700

    add CKEDITOR

--- resources/views/admin/checklists/edit.blade.php
+++ resources/views/admin/checklists/edit.blade.php
@@ -1,92 +1,108 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="fade-in">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    <form action="{{ route('admin.checklist_groups.checklists.update', [$checklistGroup, $checklist]) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        <div class="card-header">{{ __('Edit Checklist') }}</div>
+    <div class="container-fluid">
+        <div class="fade-in">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form
+                            action="{{ route('admin.checklist_groups.checklists.update', [$checklistGroup, $checklist]) }}"
+                            method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="card-header">{{ __('Edit Checklist') }}</div>
 
-                        <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input class="form-control" id="name" name="name" type="text" value="{{ $checklist->name }}">
-                                </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label for="name">Name</label>
+                                            <input class="form-control" id="name" name="name" type="text"
+                                                   value="{{ $checklist->name }}">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="card-footer">
-                            <button class="btn btn-sm btn-primary" type="submit"> {{ __('Save Checklist') }}</button>
-                        </div>
+                            <div class="card-footer">
+                                <button class="btn btn-sm btn-primary"
+                                        type="submit"> {{ __('Save Checklist') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                    <form
+                        action="{{ route('admin.checklist_groups.checklists.destroy', [$checklistGroup, $checklist]) }}"
+                        method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-sm btn-danger" type="submit"
+                                onclick="return confirm('{{ __('Are you sure ?') }}')"> {{ __('Delete This Checklist') }}</button>
                     </form>
-                </div>
-                <form action="{{ route('admin.checklist_groups.checklists.destroy', [$checklistGroup, $checklist]) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-sm btn-danger" type="submit"
-                    onclick="return confirm('{{ __('Are you sure ?') }}')"> {{ __('Delete This Checklist') }}</button>
-                </form>
 
-                <hr />
+                    <hr/>
 
-                <div class="card">
-                    <div class="card-header"><i class="fa fa-align-justify"></i> {{ __('List of Task') }}</div>
-                    <div class="card-body">
-                        @livewire('tasks-table', ['checklist' => $checklist])
+                    <div class="card">
+                        <div class="card-header"><i class="fa fa-align-justify"></i> {{ __('List of Task') }}</div>
+                        <div class="card-body">
+                            @livewire('tasks-table', ['checklist' => $checklist])
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        @if ($errors->storetask->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->storetask->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form action="{{ route('admin.checklists.tasks.store', [$checklist]) }}" method="POST">
+                            @csrf
+                            <div class="card-header">{{ __('New Task') }}</div>
+
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label for="name">{{ __('Name') }}</label>
+                                            <input class="form-control" id="name" name="name" type="text">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="description">{{ __('Description') }}</label>
+                                            <textarea class="form-control" id="task-textarea" name="description"
+                                                      rows="5"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button class="btn btn-sm btn-primary" type="submit"> {{ __('Save Task') }}</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
 
-                <div class="card">
-                    @if ($errors->storetask->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->storetask->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    <form action="{{ route('admin.checklists.tasks.store', [$checklist]) }}" method="POST">
-                        @csrf
-                        <div class="card-header">{{ __('New Task') }}</div>
-
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <div class="form-group">
-                                        <label for="name">{{ __('Name') }}</label>
-                                        <input class="form-control" id="name" name="name" type="text">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="description">{{ __('Description') }}</label>
-                                        <textarea class="form-control" id="description" name="description" rows="5"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-footer">
-                            <button class="btn btn-sm btn-primary" type="submit"> {{ __('Save Task') }}</button>
-                        </div>
-                    </form>
-                </div>
             </div>
-
         </div>
     </div>
-</div>
+@endsection
+@section('scripts')
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#task-textarea' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
 @endsection
